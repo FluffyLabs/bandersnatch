fe0129908b9ac2ceeadbfccf45666ceb63ab2f9d
Tomasz Drwięga <tomusdrw@gmail.com>
2024-07-19 07:22:43 +0200

    Build for node.

--- pkg/bandersnatch_wasm.js
+++ pkg/bandersnatch_wasm.js
@@ -1,4 +1,18 @@
-import * as wasm from "./bandersnatch_wasm_bg.wasm";
-import { __wbg_set_wasm } from "./bandersnatch_wasm_bg.js";
-__wbg_set_wasm(wasm);
-export * from "./bandersnatch_wasm_bg.js";
+let imports = {};
+let wasm;
+/**
+* @returns {boolean}
+*/
+module.exports.verify_safrole = function() {
+    const ret = wasm.verify_safrole();
+    return ret !== 0;
+};
+
+const path = require('path').join(__dirname, 'bandersnatch_wasm_bg.wasm');
+const bytes = require('fs').readFileSync(path);
+
+const wasmModule = new WebAssembly.Module(bytes);
+const wasmInstance = new WebAssembly.Instance(wasmModule, imports);
+wasm = wasmInstance.exports;
+module.exports.__wasm = wasm;
+
